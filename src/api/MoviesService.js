be0f603f1d92f261148997cee601329b5674631c
Felipe Olivares Campos <felipeolivares99@hotmail>
2022-05-12 21:13:39 -0300

    first commit

---
+++ src/api/MoviesService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API_KEY = 'ae7a4402a4febf5ff1e9c980bca1d03c';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'));
+    }
+
+    static getMovieById(id) {
+        return axios(withBaseUrl(`movie/${id}`))
+    }
+}
---
+++ src/views/About.js
@@ -0,0 +1,1 @@
+export const About = () => <h1>Página informativa</h1>
